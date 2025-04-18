ea3eec702871c2631cb05fc9a313f7e4285b8cf4
arieldelg <arieldelgrande@hotmail.com>
2025-04-18 09:28:26 -0700

    feat: add RabbitMQ integration to Auth module for microservices communication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RMQ_SERVICE } from 'src/config/services';
+import { envs } from 'src/config/envs';
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: RMQ_SERVICE,
+        transport: Transport.RMQ,
+        options: {
+          urls: envs.rmq_servers,
+          queue: 'auth',
+        },
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
